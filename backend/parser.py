9103f4913f23ffac0677bdd7789c11986eb1029e
rakotoj <johary.rakotomalala@etu.enseeiht.fr>
2022-03-30 18:51:02 +0200

    WIP data scaling

--- backend/parser.py
+++ backend/parser.py
@@ -7,14 +7,23 @@ engine = create_engine('postgresql://postgres:docker@localhost:25433/climatlas',
 
 date_parser = lambda x: datetime.strptime(x, '%Y%m%d')
 
-df = pd.read_csv(
+source_dataframe = pd.read_csv(
   '../data/temp_moyen_2m_test.txt', sep=" ", skiprows=40,
   names=['date', 'lat', 'lon', 'temp'],
   parse_dates=['date'],
   date_parser=date_parser
 )
-df['id'] = df['lat'].astype(str) + df['lon'].astype(str)
-gdf = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df.lat, df.lon))
+source_dataframe['id'] = source_dataframe['lat'].astype(str) + source_dataframe['lon'].astype(str)
+
+# For each zoom level
+for zoom_level in range(11, 17):
+  for time_scale in ['D', 'W', 'M', 'Y']:
+    # Create the corresponding data frame
+      scaled_data_frame = 
+    
+  
+
+gdf = geopandas.GeoDataFrame(source_dataframe, geometry=geopandas.points_from_xy(source_dataframe.lat, source_dataframe.lon))
 del gdf['lat']
 del gdf['lon']
 
---
+++ backend/utils/GeoUtils.py
@@ -0,0 +1,8 @@
+import math
+
+def deg2num(lat_deg, lon_deg, zoom):
+  lat_rad = math.radians(lat_deg)
+  n = 2.0 ** zoom
+  xtile = int((lon_deg + 180.0) / 360.0 * n)
+  ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+  return (xtile, ytile)
